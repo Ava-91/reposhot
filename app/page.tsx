37baf2ce1717e01b3eeb0a2751f79476865cc696
Ava <170544612+Ava-91@users.noreply.github.com>
2026-08-14 12:21:10 +0330

    chore: clean up starter page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,3 @@
-import Header from "@/components/Header";
-
 export default function Home() {
-  return (
-    <main
-      className="
-        min-h-screen
-        bg-[#09090B]
-        px-6
-        py-20
-        text-white
-      "
-    >
-      <Header />
-    </main>
-  );
+  return <main className="min-h-screen" />;
 }
